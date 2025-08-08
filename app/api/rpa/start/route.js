c24a03c7700ae39860fb66f0e949b435b4c833d3
messyandmagnetic <hello@messyandmagnetic.com>
2025-08-08 16:54:14 -0600

    commit and push

--- app/api/rpa/start/route.js
+++
@@ -1,8 +0,0 @@
-export async function GET(req) {
-  // simple alias to start-view (kept for convenience)
-  const url = new URL(req.url);
-  url.pathname = '/api/rpa/start-view';
-  const res = await fetch(url, { method: 'GET', headers: {} });
-  const body = await res.text();
-  return new Response(body, { status: res.status, headers: { 'Content-Type': 'application/json' } });
-}
