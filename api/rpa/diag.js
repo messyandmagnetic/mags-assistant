bae620c6557d8df43b974af6dbda07e2e7fb3f48
messyandmagnetic <hello@messyandmagnetic.com>
2025-08-08 23:16:10 -0600

    feat: allow ttl in rpa start session

--- api/rpa/diag.js
+++ api/rpa/diag.js
@@ -1,3 +1,4 @@
 export default async function handler(req, res) {
-  return res.status(200).json({ ok: true });
+  const token = process.env.BROWSERLESS_API_KEY || process.env.BROWSERLESS_TOKEN;
+  return res.status(200).json({ ok: true, haveKey: Boolean(token) });
 }
